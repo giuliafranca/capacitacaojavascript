195b04eaba73098705cced4ebbedfb6e46e52136
giuliafranca <82042116+giuliafranca@users.noreply.github.com>
2022-11-17 15:11:58 -0300

    comecando fundamentos

---
+++ fundamentos/tipagemfrca.js
@@ -0,0 +1,14 @@
+let qualquer = 'Legal'
+console.log(qualquer)
+console.log(typeof qualquer)
+
+qualquer = 3.1415
+console.log(qualquer)
+console.log(typeof qualquer)
+
+// evitar nome genericos e siglas
+
+// nao faz sentido:
+let valor = '' //que valor?
+let numero = 1 // numero pra que?
+let pqp = false  //que sigla é esse??? pqp
---
+++ fundamentos/variaveiseconstantes.js
@@ -0,0 +1,6 @@
+let b = 0
+var a = 15
+// comandos para declarar variaveis
+
+const c = 3
+// comando para declarar constantes
